7abed8fa607df3687335b71f053dc1757dbacd07
NDAM TAYOU DANIEL ANDY MAEL <danielandymael@gmail.com>
2026-04-24 09:18:28 +0000

    class main cree

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ContactsManager myContactsManager = new ContactsManager();
+
+        Contact c1 = new Contact();
+        c1.name = "Alice";
+        c1.phoneNumber = "0102030405";
+        myContactsManager.addContact(c1);
+
+        Contact c2 = new Contact();
+        c2.name = "Bob";
+        c2.phoneNumber = "0542300950";
+        myContactsManager.addContact(c2);
+
+        Contact c3 = new Contact();
+        c3.name = "Charlie";
+        c3.phoneNumber = "0705024759";
+        myContactsManager.addContact(c3);
+
+        Contact c4 = new Contact();
+        c4.name = "Diane";
+        c4.phoneNumber = "0566778899";
+        myContactsManager.addContact(c4);
+
+        Contact c5 = new Contact();
+        c5.name = "Eric";
+        c5.phoneNumber = "0188776655";
+        myContactsManager.addContact(c5);
+
+        Contact result = myContactsManager.searchContact("Charlie");
+        if (result != null) {
+            System.out.println("Numéro de Charlie : " + result.phoneNumber);
+        } else {
+            System.out.println("Contact non trouvé.");
+        }
+    }
+}
